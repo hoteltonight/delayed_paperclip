c87b756e942220c385d46ba5a7f9b18ff3a956ad
Dennis Suratna <dsuratna@unknown68a86d05e5ae.(none)>
2013-04-08 11:49:17 -0700

    Process customer_photos in a separate queue

--- lib/delayed_paperclip/jobs/resque.rb
+++ lib/delayed_paperclip/jobs/resque.rb
@@ -7,7 +7,13 @@ module DelayedPaperclip
       @queue = :paperclip
 
       def self.enqueue_delayed_paperclip(instance_klass, instance_id, attachment_name)
-        ::Resque.enqueue(self, instance_klass, instance_id, attachment_name)
+        queue = case instance_klass
+                when "CustomerPhoto" then :customer_photo
+                else
+                  @queue
+                end
+
+        ::Resque.enqueue_to(queue, self, instance_klass, instance_id, attachment_name)
       end
 
       def self.perform(instance_klass, instance_id, attachment_name)
