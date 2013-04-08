d14eb249a1296a3622d72c8368b3a2d1ef040489
Dennis Suratna <dsuratna@unknown68a86d05e5ae.(none)>
2013-04-08 11:49:17 -0700

    Conditional usage of queue

--- lib/delayed_paperclip/jobs/resque.rb
+++ lib/delayed_paperclip/jobs/resque.rb
@@ -7,7 +7,11 @@ module DelayedPaperclip
       @queue = :paperclip
 
       def self.enqueue_delayed_paperclip(instance_klass, instance_id, attachment_name)
-        ::Resque.enqueue(self, instance_klass, instance_id, attachment_name)
+        if instance_klass.to_s == "CustomerPhoto"
+          ::Resque.enqueue_to("customer_photo", self, instance_klass, instance_id, attachment_name)
+        else
+          ::Resque.enqueue(self, instance_klass, instance_id, attachment_name)
+        end
       end
 
       def self.perform(instance_klass, instance_id, attachment_name)
